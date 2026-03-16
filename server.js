f30dbb61d18f35705e133f4dd5752b99a86d4178
Stiven-Longas <longasstiven829@gmail.com>
2026-03-15 20:16:53 -0500

    cors manual headers

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express  = require("express");
+const express    = require("express");
 const nodemailer = require("nodemailer");
-const cors     = require("cors");
 require("dotenv").config();
 
 const app  = express();
 const PORT = process.env.PORT || 4000;
 
-// Middlewares
-app.use(cors());
+// CORS manual
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 // Configurar transporter de Gmail
@@ -56,7 +64,3 @@ app.post("/api/contacto", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-
-
-
